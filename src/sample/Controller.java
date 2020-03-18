a7d1cc0f34b9a7f01621063c7c42e1c4deb08d0d
Ismet Perenda <iperenda1@etf.unsa.ba>
2020-03-18 19:24:32 +0100

    tutorijal 2,zadatak 1 Dugme sa Labelom Say Hello

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+
+
+public class Controller {
+    public javafx.scene.control.Label Label;
+
+    public void Klikni(ActionEvent actionEvent) {
+        Label.setText("HellooWorld");
+    }
+}
